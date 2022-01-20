fd956c58857b4723ef16650dfcc11836c31ae75f
Jongjunp <pjjkey@kaist.ac.kr>
2022-01-20 13:08:49 +0000

    temp store

--- routes/bookmark.js
+++ routes/bookmark.js
@@ -5,29 +5,29 @@ const request = require('request');
 var Bookmarks = require('../models/bookmark');
 
 //show all the bookmarks
-router.get('/bookmarks/1', function(req,res) {
+router.get('/bookmark/1', function(req,res) {
     Bookmarks.find(req.body.nickname);
     Bookmarks
 });
 
 //add bookmarks
-router.post('/bookmarks/2', function(req,res) {
+router.post('/bookmark/2', function(req,res) {
     var localNickname = req.body.nickname;
     var localEmail = req.body.email;
 });
 
 //put memo
-router.put('/bookmarks/memo/1', function(req,res) { 
+router.put('/bookmark/memo/1', function(req,res) { 
     var localNickname = req.body.memo;
 });
 
 //modify memo
-router.put('/bookmarks/memo/2', function(req,res) {
+router.put('/bookmark/memo/2', function(req,res) {
     var
 });
 
 //delete bookmark
-router.delete('/bookmarks/3', function(req,res) {
+router.delete('/bookmark/3', function(req,res) {
     var 
 });
 
--- routes/recommend.js
+++ routes/recommend.js
@@ -6,25 +6,42 @@ const url = require('url');
 const request = require('request');
 const fs = require('fs');
 var Recommend = require('../models/recommend');
-
+var Bookmark = require('../models/bookmark');
+var recommendList = new Array();
 
 //google 검색 api 이용 추천 bookmark list 전달
 const client = new GoogleSearch('dc4f1ca011bbda57e','AIzaSyDq578MkaY3XqSxvYULBaszsg_shordbAA');
 const pageStVal = 1;
 
+//get keyword
+router.get('/bookmark/keyword', function(req,res) {
+    var localEmail = req.body.email;
+    var localOriginLink = req.body.originlink;
+    
+    Bookmark.findOne({ email:,}
+
+    )
+
+})
+
+
 //Search
 const searchFunc = (keyword, pageStVal) => {
     client.search(keyWord,  {page: pageStVal, size: 'large'}).then(images => {
         images.forEach(img => {
-            img.url
+            recommendList[index] = img.url;
     }).catch(error => {
         console.log(">> "+error);
         console.log("Collected Every Images");
         return;
     });
-}
+})}
 
-router.get()
+recommendList.forEach(
+    router.post('/recommend/list', function(req,res) {
+
+    })
+);
 
 
 module.exports = router;
